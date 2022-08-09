2e3b48cfccfd3e4ffb2bdb57662b24cda611284e
Pio <[omitted]>
2022-08-09 23:07:16 +0000

    fix highlighter's position

--- NEAT_usability/gui/highlighter.gd
+++ NEAT_usability/gui/highlighter.gd
@@ -5,21 +5,29 @@ extends Node2D
 object (the agent body).
 """
 
+var cell_position = Vector2(0,0)
+
 func _ready() -> void:
-    # set the name
-    set_name("Highlighter_" + get_parent().name)
-    # draw the circe, then hide it (otherwise every body would have a circle by default)
-    update()
-    hide()
+	# set the name
+	set_name("Highlighter_" + get_parent().name)
+	# draw the circe, then hide it (otherwise every body would have a circle by default)
+	update()
+	hide()
+
+
+func _process(delta):
+	if !get_parent().is_dead:
+		cell_position = get_parent().get_node("Cell").position
+		update()
 
 
 func _draw():
-    """Draw a circle around the parent node.
-    """
-    draw_arc(Params.highlighter_offset,
-             Params.highlighter_radius,
-             0,
-             TAU,
-             Params.highlighter_radius / 2,
-             Params.highlighter_color,
-             Params.highlighter_width)
+	"""Draw a circle around the parent node.
+	"""
+	draw_arc(cell_position,
+			 Params.highlighter_radius,
+			 0,
+			 TAU,
+			 Params.highlighter_radius / 2,
+			 Params.highlighter_color,
+			 Params.highlighter_width)
